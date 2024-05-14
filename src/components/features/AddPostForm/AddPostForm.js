570015840f4bdd3040dbc9e11d97b25a9f3b2a51
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-05-14 15:36:31 +0200

    fix AddPostForm layout

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -25,40 +25,43 @@ const AddPostForm = (props) => {
         setContent('');
         navigate('/');
     };
-
+//<Row className="justify-content-center">
+//<Col className="col-lg-6">
     return (
-    <Row className="mx-5">
-        <div className="mb-2">
-            <h1>Add post</h1>
-        </div>
-        <Form onSubmit={handleSubmit}>
-            <Col>
-                <Form.Group className="mb-3" controlId="title">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control className="w-50" type="text" placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="author">
-                    <Form.Label>Author</Form.Label>
-                    <Form.Control className="w-50" type="text" placeholder="Enter author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="published">
-                    <Form.Label>Published</Form.Label>
-                    <Form.Control className="w-50" type="text" placeholder="dd-mm-yyyy" value={publishedDate} onChange={(e) => setPublishedDate(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Label>Short description</Form.Label>
-                    <Form.Control as="textarea" rows={5} placeholder="Short description" value={shortDescription} onChange={(e) => setShortDescription(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="content">
-                    <Form.Label>Main content</Form.Label>
-                    <Form.Control as="textarea" rows={10} placeholder="Main content" value={content} onChange={(e) => setContent(e.target.value)} />
-                </Form.Group>
-            </Col>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-        </Form>
-    </Row>
+    <section>
+            <Row className="justify-content-center">
+                <Col className="col-lg-6">
+                    <div className="mb-4">
+                        <h1>Add post</h1>
+                    </div>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control className="w-50" type="text" placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="author">
+                            <Form.Label>Author</Form.Label>
+                            <Form.Control className="w-50" type="text" placeholder="Enter author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="published">
+                            <Form.Label>Published</Form.Label>
+                            <Form.Control className="w-50" type="text" placeholder="dd-mm-yyyy" value={publishedDate} onChange={(e) => setPublishedDate(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="description">
+                            <Form.Label>Short description</Form.Label>
+                            <Form.Control as="textarea" rows={5} placeholder="Short description" value={shortDescription} onChange={(e) => setShortDescription(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="content">
+                            <Form.Label>Main content</Form.Label>
+                            <Form.Control as="textarea" rows={10} placeholder="Main content" value={content} onChange={(e) => setContent(e.target.value)} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+    </section>
 
     );
 };
